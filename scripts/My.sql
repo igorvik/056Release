b7577caebea924587a134e36505edd50dc38a71e
ihorvik <ihor.ilnytskyi@gmail.com>
2015-07-16 11:34:04 +0300

    oms

---
+++ scripts/My.sql
@@ -0,0 +1,19 @@
+show create table OrderItems;
+
+show create table Orders;
+
+show create table Users;
+
+show create table Products;
+
+show create table CustomerTypes;
+
+show create table Dimensions;
+
+show create table OrderStatuses;
+
+show create table Regions;
+
+show create table Roles;
+
+show create table CardType;
---
+++ scripts/dropDataMySql.sql
@@ -0,0 +1,26 @@
+delete from  `oms`.`OrderItems`;
+alter table OrderItems auto_increment=1;
+
+delete from  `oms`.`Orders`;
+alter table Orders auto_increment=1;
+
+delete from   `oms`.`Users`;
+alter table Users auto_increment=1;
+
+delete from  `oms`.`Products`;
+alter table Products auto_increment=1;
+
+delete from  `oms`.`CustomerTypes`;
+alter table CustomerTypes auto_increment=1;
+
+delete from `oms`.`Dimensions`;
+alter table Dimensions auto_increment=1;
+
+delete from  `oms`.`OrderStatuses`;
+alter table OrderStatuses auto_increment=1;
+
+delete from  `oms`.`Regions`;
+alter table Regions auto_increment=1;
+
+delete from  `oms`.`Roles`;
+alter table Roles auto_increment=1;
